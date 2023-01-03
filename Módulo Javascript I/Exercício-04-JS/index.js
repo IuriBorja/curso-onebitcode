b887262d0e63ba2e06365e6cec765f9454b80847
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-01-03 23:40:00 +0000

    att

--- Módulo Javascript I/Exercício-04-JS/index.js
+++ Módulo Javascript I/Exercício-04-JS/index.js
@@ -13,18 +13,50 @@ visitou
 */
 
 
-const nomeTurista = prompt("Diga seu nome:");
+
+
+const turista = prompt("Diga seu nome:");
+
+let pergunta = prompt("Voce já visitou alguma cidade? (sim/não)");
+
+let cidades = "";
+let contagem = 0;
+
+
+while (pergunta === "sim") {
+    let cidade = prompt("Qual cidade visitou?");
+    cidades += " - " + cidade + "\n";
+    contagem++;
+    pergunta = prompt("Já visitou outras? (sim/não)");
+
+    if (pergunta === "não") {
+        alert("Ok, já não tenho mais perguntas!");
+        break;
+    }
+}
+
+
+alert(
+    `Nome do turista: ${turista} \n
+    Quantidade de cidades visitadas: ${contagem} \n
+    Quais cidades visitadas: \n${cidades}`
+);
+
+
+
+
+/* const nomeTurista = prompt("Diga seu nome:");
 
 let visitouCidade = prompt("Já visitou alguma cidade? (sim/não)");
 
 
-let visitouCidade2 = 0;
+let contagem = 0;
 let cidades = "";
 
 while (visitouCidade === "sim") {
     let cidade = prompt("Qual a cidade?");
-    cidades += " - " + cidade + "\n"
-    visitouCidade2++;
+    cidades += " - " + cidade + "\n";
+    contagem++;
     visitouCidade = prompt("Já visitou outras?");
 
     if (visitouCidade === "não") {
@@ -34,4 +66,7 @@ while (visitouCidade === "sim") {
 }
 
 
-alert(`Nome do turista: ${nomeTurista} \nQuantas cidades visitadas: ${visitouCidade2} \nQuais cidades ele visitou: \n ${cidades}`);
+
+alert(`Nome do turista: ${nomeTurista}
+\nQuantas cidades visitadas: ${contagem} 
+\nQuais cidades ele visitou: \n ${cidades}`); */
