3621fa607dc003a80481a1db76d73b54f4acbd9e
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-01-07 16:06:41 +0000

    att aula/class obj js

---
+++ Módulo Javascript I/13-Objetos/script.js
@@ -0,0 +1,35 @@
+const obj = {
+    nome: "Iuri",
+    idade: 32,
+    cabelo: true,
+    carro: null,
+    rico: undefined,
+    estudo : ["HTML", "CSS", "tempo", {
+        estudo_html: 10,
+        estudo_css: 5,
+    }]
+};
+
+
+
+console.log(obj);
+
+
+
+const pessoa = {};
+
+pessoa.nome = "Iuri";
+pessoa.idade = 32;
+pessoa.colegas = ["Mariazinha", "Joazin", "Astolfo"];
+
+pessoa.endereço = {
+    rua: "Sei la",
+    numero: 2,
+    bairro: "não"
+};
+
+console.log(pessoa);
+
+
+
+
