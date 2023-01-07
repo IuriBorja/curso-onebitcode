dc2d3083ad3b91ee7e272c16883f304c645ff196
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-01-07 13:19:27 +0000

    att exerc array

--- Módulo Javascript I/Exercício-09-JS/script.js
+++ Módulo Javascript I/Exercício-09-JS/script.js
@@ -1,4 +1,4 @@
-let nomes = ["Jonh ", "Chad ", "Maria ", "Anthony ", "Flea "];
+/* let nomes = ["Jonh ", "Chad ", "Maria ", "Anthony ", "Flea "];
 
 let lista = [];
 
@@ -25,4 +25,51 @@ do {
     }
 
 } while (lista !== "c")
+ */
 
+
+
+
+
+/* MODO DO PROFESSOR */
+
+
+
+
+let fila = [];
+let opcao = "";
+
+do {
+    let pacientes = "";
+    for(let i = 0; i < fila.length; i++) {
+        pacientes += (i + 1) + "ª - " + fila[i] + "\n"; 
+    }
+
+    opcao = prompt(
+        "Pacientes:\n" + pacientes +
+        "\nEscolha uma opção:\n1. Novo paciente\n2. Consultar paciente\n3. Sair"
+    )
+
+        switch (opcao) {
+            case "1":
+                const novoPaciente = prompt("Qual é o nome do paciente?");
+                fila.push(novoPaciente);
+                break;
+            case "2":
+                const pacienteConsultado = fila.shift();
+                if (pacienteConsultado) {
+                alert(pacienteConsultado + " foi removido da fila.");
+                } else {
+                    alert("Não há mais pacientes na fila!");
+                }    
+                break;
+            case "3":
+                alert("Encerrando...");
+                break;
+            default:
+                alert("Opção inválida!");
+                break;            
+        }
+
+
+} while (opcao !== "3")
