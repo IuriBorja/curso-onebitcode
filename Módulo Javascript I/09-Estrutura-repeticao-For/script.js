ff480b1da149ec2e8792a70fdd911d1b86ee42f1
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-01-26 14:08:42 +0000

    att

--- Módulo Javascript I/09-Estrutura-repeticao-For/script.js
+++ Módulo Javascript I/09-Estrutura-repeticao-For/script.js
@@ -17,11 +17,58 @@ for (let i = 0; i < nome.length; i++) {
 /* --------------- */
 
 
-let array = ["frio", "calor",
+/* let array = ["frio", "calor",
 "gelado", "quente", "irineu", 
-"jujuba", "faustao:comendo:pé:de:moleque"];
+"jujuba", "fulano@email.com:22345621"];
 
 
 for (let i = 0; i < array.length; i++) {
-    console.log(array[6].split(":"));
+    let info = array[6].split(":");
+    console.log(info);
+} */
+
+/* console.log(info); */
+
+
+
+/* 
+let array = ["belno@email.com:22345", "ano@email.com:621",
+"fu@email.com:22345", "lan@email.com:25621", "ifulano@email.com:22345621", "fulano@email.com:22345621"];
+
+let objArray = array.map(info => {
+  let data = info.split(":");
+  return {
+    email: data[0],
+    senha: data[1]
+  }
+});
+
+console.log(objArray);
+ */
+
+
+ let array = ["belno@email.com:22345", "ano@email.com:621", "fu@email.com:22345", "lan@email.com:25621", "ifulano@email.com:22345621", "fulano@email.com:22345621"];
+
+let objetos = [];
+
+for (let i = 0; i < array.length; i++) {
+    let info = array[i].split(":");
+    let objeto = { email: info[0], senha: info[1] };
+    objetos.push(objeto);
+}
+
+console.log(objetos);
+
+
+
+
+
+
+let array2 = ["belno@email.com:22345", "ano@email.com:621", "fu@email.com:22345", "lan@email.com:25621", "ifulano@email.com:22345621", "fulano@email.com:22345621"];
+
+let info = [];
+
+for (let i = 0; i < array2.length; i++) {
+    info += array2[i];
+    console.log(info);
 }
