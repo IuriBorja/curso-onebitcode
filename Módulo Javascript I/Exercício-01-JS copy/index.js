8393c63403012c4687a78a8f46838f13a0e11a13
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-01-28 13:50:59 +0000

    att exerc

---
+++ Módulo Javascript I/Exercício-01-JS copy/index.js
@@ -0,0 +1,32 @@
+let primeiroNome = prompt("Digite seu primeiro nome: ");
+let sobreNome = prompt("Digite seu sobre nome: ");
+let campoEstudo = prompt("Digite seu campo de estudo: ");
+let anoNascimento = prompt("Digite seu ano de nascimento: ");
+
+
+let result = alert(`
+Primeiro nome: ${primeiroNome}\n
+Sobrenome: ${sobreNome}\n
+Campo de Estudo: ${campoEstudo}\n
+Ano de Nascimento: ${anoNascimento}`);
+
+
+
+/* ------------------------- */
+
+
+
+let num1 = parseFloat(prompt("Digite um número: "));
+let num2 = parseFloat(prompt("Digite outro número: "));
+
+
+
+let resultado = alert(`
+Resultado soma: ${num1 + num2}\n
+Resultado subtração: ${num1 - num2}\n
+Resultado multiplicação: ${num1 * num2}\n
+Resultado divisão: ${num1 / num2}`);
+
+
+
+
