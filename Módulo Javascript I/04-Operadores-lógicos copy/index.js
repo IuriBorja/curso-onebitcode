c8ffb264efa9c987d461bc9fc0f97f5bcf5dfb8e
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-03-16 13:50:55 +0000

    att

---
+++ Módulo Javascript I/04-Operadores-lógicos copy/index.js
@@ -0,0 +1,28 @@
+
+
+
+for (var i = 0; i < accounts.length; i++) {
+    var accountData = accounts[i].split(":");
+    var password = toolkit.rot13(accountData[1]);
+    var account = { username: accountData[0], password: password };
+    accounts[i] = account;
+}
+
+console.log(accounts);
+
+var next = function(description) {
+    console.log(description);
+};
+
+
+toolkit.bruteForce(terminal, accounts, function(username, password) {
+    console.log(username, password);
+    var success = terminal.login(username, password);
+    toolkit.spy(terminal, "root", next);
+});
+
+
+
+function () {
+    
+}
